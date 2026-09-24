f67d5857842b933861d2b537ba4a8fd76ddbe303
sahilkg007 <sahilkg007@gmail.com>
2026-09-25 02:04:15 +0530

    Update: longest-consecutive-sequence - submission-3

--- Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -1,13 +1,22 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
 
-        for num in nums:
-            if num-1 not in numSet:
-                length = 1
-                while(num+length) in numSet:
-                    length += 1
-                    longest = max(length , longest)
-            
-        return longest
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+
+                if (board[i][j] in rows[i] or
+                    board[i][j] in cols[j] or
+                    board[i][j] in squares[(i//3),(j//3)]):
+                    return False
+                
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                squares[(i//3,j//3)].add(board[i][j])
+        return True
+
+                    
